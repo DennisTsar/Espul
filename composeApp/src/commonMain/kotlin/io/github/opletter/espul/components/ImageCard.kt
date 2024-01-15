9dc69bbe1d4fa60fa53d9e2b59634d8f638083ba
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-01-15 02:26:20 -0500

    Simplify ImageCard

--- composeApp/src/commonMain/kotlin/io/github/opletter/espul/components/ImageCard.kt
+++ composeApp/src/commonMain/kotlin/io/github/opletter/espul/components/ImageCard.kt
@@ -6,18 +6,24 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import coil3.compose.LocalPlatformContext
 import coil3.request.ImageRequest
 
 @Composable
-fun ImageCard(url: String?, onClick: () -> Unit, modifier: Modifier = Modifier) {
+fun ImageCard(
+    url: String?,
+    label: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
     Card(
         onClick = onClick,
         modifier = modifier,
@@ -25,39 +31,34 @@ fun ImageCard(url: String?, onClick: () -> Unit, modifier: Modifier = Modifier) 
             defaultElevation = 10.dp
         )
     ) {
-        Box(Modifier.fillMaxSize()) {
-            var imageLoaded by remember { mutableStateOf(false) }
+        Box(contentAlignment = Alignment.BottomStart) {
             AsyncImage(
                 model = ImageRequest.Builder(LocalPlatformContext.current)
                     .data(url)
                     .build(),
                 contentDescription = null,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier.fillMaxSize(),
-                onSuccess = { imageLoaded = true },
             )
-            if (imageLoaded) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.25f)
-                        .align(Alignment.BottomCenter)
-                        .background(
-                            brush = Brush.verticalGradient(
-                                colors = listOf(
-                                    Color.Transparent,
-                                    Color.Black.copy(alpha = 0.85f),
-                                )
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.25f)
+                    .align(Alignment.BottomCenter)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black.copy(alpha = 0.65f),
                             )
                         )
-                )
-            }
+                    )
+            )
             Text(
-                text = "Hello World",
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(12.dp),
+                text = label,
+                modifier = Modifier.padding(6.dp),
                 color = Color.White,
-                fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
             )
         }
     }
